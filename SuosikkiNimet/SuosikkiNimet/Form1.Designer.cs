663dad66aa5e77c37a7678f9507bea4ee65d6442
Jimi Lindfors <70505822+Velmu1@users.noreply.github.com>
2021-11-21 23:07:01 +0200

    Add files via upload

---
+++ SuosikkiNimet/SuosikkiNimet/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace SuosikkiNimet
+{
+    partial class SuosikkiForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NimiTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.TarkastBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(539, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna nimesi, niin tarkastan onko se 50 suosituimman joukossa:";
+            // 
+            // NimiTB
+            // 
+            this.NimiTB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NimiTB.Location = new System.Drawing.Point(617, 30);
+            this.NimiTB.Name = "NimiTB";
+            this.NimiTB.Size = new System.Drawing.Size(100, 33);
+            this.NimiTB.TabIndex = 1;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VastausLB.Location = new System.Drawing.Point(37, 78);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 25);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // TarkastBT
+            // 
+            this.TarkastBT.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TarkastBT.Location = new System.Drawing.Point(617, 79);
+            this.TarkastBT.Name = "TarkastBT";
+            this.TarkastBT.Size = new System.Drawing.Size(100, 38);
+            this.TarkastBT.TabIndex = 3;
+            this.TarkastBT.Text = "Tarkasta";
+            this.TarkastBT.UseVisualStyleBackColor = true;
+            this.TarkastBT.Click += new System.EventHandler(this.TarkastBT_Click);
+            // 
+            // SuosikkiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 149);
+            this.Controls.Add(this.TarkastBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.NimiTB);
+            this.Controls.Add(this.label1);
+            this.Name = "SuosikkiForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NimiTB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button TarkastBT;
+    }
+}
+
