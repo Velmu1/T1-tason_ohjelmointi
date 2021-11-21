5137fbd121fb4307ffc675545fa271bf161afaa8
Jimi Lindfors <70505822+Velmu1@users.noreply.github.com>
2021-11-21 22:56:45 +0200

    Add files via upload

---
+++ BMI-laskuri/BMI-laskuri/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace BMI_laskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PainoTB = new System.Windows.Forms.TextBox();
+            this.PituusTB = new System.Windows.Forms.TextBox();
+            this.BMIBT = new System.Windows.Forms.Button();
+            this.BmiLB = new System.Windows.Forms.Label();
+            this.KuvausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(154, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna paino:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(154, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna pituus:";
+            // 
+            // PainoTB
+            // 
+            this.PainoTB.Location = new System.Drawing.Point(304, 175);
+            this.PainoTB.Name = "PainoTB";
+            this.PainoTB.Size = new System.Drawing.Size(100, 23);
+            this.PainoTB.TabIndex = 2;
+            // 
+            // PituusTB
+            // 
+            this.PituusTB.Location = new System.Drawing.Point(304, 214);
+            this.PituusTB.Name = "PituusTB";
+            this.PituusTB.Size = new System.Drawing.Size(100, 23);
+            this.PituusTB.TabIndex = 3;
+            // 
+            // BMIBT
+            // 
+            this.BMIBT.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BMIBT.Location = new System.Drawing.Point(154, 257);
+            this.BMIBT.Name = "BMIBT";
+            this.BMIBT.Size = new System.Drawing.Size(250, 38);
+            this.BMIBT.TabIndex = 4;
+            this.BMIBT.Text = "Laske painoindeksi";
+            this.BMIBT.UseVisualStyleBackColor = true;
+            this.BMIBT.Click += new System.EventHandler(this.BMIBT_Click);
+            // 
+            // BmiLB
+            // 
+            this.BmiLB.AutoSize = true;
+            this.BmiLB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BmiLB.Location = new System.Drawing.Point(154, 331);
+            this.BmiLB.Name = "BmiLB";
+            this.BmiLB.Size = new System.Drawing.Size(68, 30);
+            this.BmiLB.TabIndex = 5;
+            this.BmiLB.Text = "label3";
+            this.BmiLB.Visible = false;
+            // 
+            // KuvausLB
+            // 
+            this.KuvausLB.AutoSize = true;
+            this.KuvausLB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.KuvausLB.Location = new System.Drawing.Point(154, 365);
+            this.KuvausLB.Name = "KuvausLB";
+            this.KuvausLB.Size = new System.Drawing.Size(68, 30);
+            this.KuvausLB.TabIndex = 6;
+            this.KuvausLB.Text = "label3";
+            this.KuvausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 615);
+            this.Controls.Add(this.KuvausLB);
+            this.Controls.Add(this.BmiLB);
+            this.Controls.Add(this.BMIBT);
+            this.Controls.Add(this.PituusTB);
+            this.Controls.Add(this.PainoTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PainoTB;
+        private System.Windows.Forms.TextBox PituusTB;
+        private System.Windows.Forms.Button BMIBT;
+        private System.Windows.Forms.Label BmiLB;
+        private System.Windows.Forms.Label KuvausLB;
+    }
+}
+
---
+++ CelciusToFahrenheit/CelciusToFahrenheit/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace CelciusToFahrenheit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.CelsiusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(168, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna asteet:";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VastausLB.Location = new System.Drawing.Point(168, 245);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(83, 30);
+            this.VastausLB.TabIndex = 1;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // CelsiusRB
+            // 
+            this.CelsiusRB.AutoSize = true;
+            this.CelsiusRB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CelsiusRB.Location = new System.Drawing.Point(441, 183);
+            this.CelsiusRB.Name = "CelsiusRB";
+            this.CelsiusRB.Size = new System.Drawing.Size(95, 34);
+            this.CelsiusRB.TabIndex = 2;
+            this.CelsiusRB.TabStop = true;
+            this.CelsiusRB.Text = "Celsius";
+            this.CelsiusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FahrenheitRB.Location = new System.Drawing.Point(441, 222);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(128, 34);
+            this.FahrenheitRB.TabIndex = 3;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(441, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Miten muunnat?";
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MuunnaBT.Location = new System.Drawing.Point(648, 183);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(113, 45);
+            this.MuunnaBT.TabIndex = 5;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = true;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.Location = new System.Drawing.Point(314, 178);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(100, 23);
+            this.AsteetTB.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 623);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FahrenheitRB);
+            this.Controls.Add(this.CelsiusRB);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.RadioButton CelsiusRB;
+        private System.Windows.Forms.RadioButton FahrenheitRB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button MuunnaBT;
+        private System.Windows.Forms.TextBox AsteetTB;
+    }
+}
+
